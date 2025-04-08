ce2f1ad1c4bd5540d99cdeeccc8c558ae3442abc
JearlDev <jearlwebdesign@gmail.com>
2025-04-08 13:11:14 +0200

    updated app.vue

--- app.vue
+++ app.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container text-center py-10">
     <h1>
-      Site domain: {{ $msr('{SITE_DOMAIN}') }}<br>
-      Site name: {{ $msr('{SITE_NAME}') }}
+      Site domain: {{ SITE_DOMAIN }}<br />
+      Site name: {{ SITE_NAME }}
     </h1>
   </div>
 </template>
 
 <script setup>
+const config = useRuntimeConfig();
+const SITE_DOMAIN = config.public.siteDomain;
+const SITE_NAME = config.public.siteName;
+
 useSeoMeta({
-  title: 'hi {SITE_DOMAIN} // {SITE_NAME}',
-})
+  title: "hi {SITE_DOMAIN} // {SITE_NAME}",
+});
 </script>
