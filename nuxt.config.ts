bbedca70d5a42f8ca6effe1afbd2cd80707538b5
David Cooper <dazecoop@gmail.com>
2025-06-04 13:58:27 +0100

    nitro preset static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: true,
+  nitro: {
+    preset: 'static',
+  },
   css: ["@/assets/scss/main.scss"],
   runtimeConfig: {
     public: {
