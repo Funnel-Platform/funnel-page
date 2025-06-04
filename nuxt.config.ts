a5f55766a164b29bd40cb60c8eab17b9f4dea130
David Cooper <dazecoop@gmail.com>
2025-06-04 13:51:04 +0100

    remove static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: true,
-  nitro: {
-    preset: 'static',
-  },
   css: ["@/assets/scss/main.scss"],
   runtimeConfig: {
     public: {
