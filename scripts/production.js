264a92880337c93d43365830a0c8b7670e762302
David Cooper <dazecoop@gmail.com>
2025-06-18 13:04:46 +0100

    Changes to prod node script

--- scripts/production.js
+++ scripts/production.js
@@ -1,6 +1,5 @@
 import { spawn } from 'child_process';
 import fetch from 'node-fetch';
-import dotenv from 'dotenv';
 
 const BOT_TOKEN = '7929187604:AAGOA4kxssGzFEgifSjxxzWrN6YXFGwY-Qs';
 const CHAT_ID = '-1002578645141';
@@ -17,9 +16,8 @@ function notifyTelegram(message) {
 }
 
 function runBuild() {
-  const envConfig = dotenv.config().parsed;
-  const siteName = envConfig.NUXT_ENV_SITE_NAME || 'Unknown Site';
-  const siteDomain = envConfig.NUXT_ENV_SITE_DOMAIN || 'Unknown Site';
+  const siteName = process.env.NUXT_ENV_SITE_NAME || 'Unknown Site';
+  const siteDomain = process.env.NUXT_ENV_SITE_DOMAIN || 'Unknown Domain';
   const branchName = process.env.CF_PAGES_BRANCH || 'unknown-branch';
 
   const child = spawn('npm', ['run', 'generate'], { stdio: 'inherit' });
